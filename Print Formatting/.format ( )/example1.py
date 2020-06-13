49cedfa194810e32a84891214f19656e63b7a632
Rohit Prasan Mandal <50553530+xiaowuc2@users.noreply.github.com>
2020-06-13 14:23:44 +0530

    Update example1.py

--- Print Formatting/.format ( )/example1.py
+++ Print Formatting/.format ( )/example1.py
@@ -1,3 +1,9 @@
 print("I have {} {} {}".format("go","went","gone"))
 > I have go went gone
 
+print("I have {} {} ".format("go","went","gone"))
+> I have go went
+
+print("I have {}".format("go","went","gone"))
+> Error
+
